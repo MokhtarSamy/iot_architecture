59502151903e828b68d5324be73d723823228317
Mokhtar SAMY <60510677+MokhtarSamy@users.noreply.github.com>
2023-01-14 18:58:43 +0100

    comments for Raspberry pi implementation

--- reconnaissance_faciale.py
+++ reconnaissance_faciale.py
@@ -1,17 +1,13 @@
 import face_recognition
 import cv2
 import numpy as np
+# import picamera
 
-# This is a demo of running face recognition on live video from your webcam. It's a little more complicated than the
-# other example, but it includes some basic performance tweaks to make things run a lot faster:
-#   1. Process each video frame at 1/4 resolution (though still display it at full resolution)
-#   2. Only detect faces in every other frame of video.
+# https://github.com/ageitgey/face_recognition/blob/master/examples/facerec_on_raspberry_pi.py
+# Sur le raspberry pi, il faut utilisr camera = picamera.PiCamera()
+camera = cv2.VideoCapture(0)
 
-# PLEASE NOTE: This example requires OpenCV (the `cv2` library) to be installed only to read from your webcam.
-# OpenCV is *not* required to use the face_recognition library. It's only required if you want to run this
-# specific demo. If you have trouble installing it, try any of the other demos that don't require it instead.
-
-video_capture = cv2.VideoCapture(0)
+# Sur le raspberry pi, camera.resolution = (320, 240)
 
 # à ajouter la photo de la personne à identifier
 person_image = face_recognition.load_image_file("photos_train/person.jpg")
@@ -21,7 +17,6 @@ person_face_encoding = face_recognition.face_encodings(person_image)[0]
 known_face_encodings = [person_face_encoding]
 known_face_names = ["Person"]
 
-# Initialize some variables
 face_locations = []
 face_encodings = []
 face_names = []
@@ -29,12 +24,14 @@ process_this_frame = True
 
 while True:
     # Grab a single frame of video
-    ret, frame = video_capture.read()
+    ret, frame = camera.read()
 
     # Only process every other frame of video to save time
     if process_this_frame:
         # Resize frame of video to 1/4 size for faster face recognition processing
         small_frame = cv2.resize(frame, (0, 0), fx=0.25, fy=0.25)
+
+        # Sur le raspberry pi camera.capture(output, format="rgb")
 
         # Convert the image from BGR color (which OpenCV uses) to RGB color (which face_recognition uses)
         rgb_small_frame = small_frame[:, :, ::-1]
@@ -51,11 +48,6 @@ while True:
                                                      face_encoding)
             name = "Unknown"
 
-            # # If a match was found in known_face_encodings, just use the first one.
-            # if True in matches:
-            #     first_match_index = matches.index(True)
-            #     name = known_face_names[first_match_index]
-
             # Or instead, use the known face with the smallest distance to the new face
             face_distances = face_recognition.face_distance(
                 known_face_encodings, face_encoding)
@@ -67,7 +59,6 @@ while True:
 
     process_this_frame = not process_this_frame
 
-    # Display the results
     for (top, right, bottom, left), name in zip(face_locations, face_names):
         # Scale back up face locations since the frame we detected in was scaled to 1/4 size
         top *= 4
@@ -93,5 +84,5 @@ while True:
         break
 
 # Release handle to the webcam
-video_capture.release()
+camera.release()
 cv2.destroyAllWindows()
